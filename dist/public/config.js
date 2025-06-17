aba82795b8ec634a0226797336bbea9270f2d2cb
APEXPREDETOR-HIRANI <aryanhirani008@gmail.com>
2025-06-17 18:30:36 +0300

    Initial backend setup for Render deployment

---
+++ dist/public/config.js
@@ -0,0 +1,4 @@
+window.ENV = {
+    SUPABASE_URL: 'https://awcfbhamqbqyuuqhtage.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3Y2ZiaGFtcWJxeXV1cWh0YWdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMTg0MzcsImV4cCI6MjA2NDc5NDQzN30.L6dTE2DTjWoldYjL4SQAVQVbVho5DFlS04O7qXRgQzw'
+}; 
